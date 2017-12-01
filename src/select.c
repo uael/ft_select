c2da983f58d85240199787625410c2bfc7c3362a
alucas- <alucas-@student.42.fr>
2017-12-01 12:42:51 +0100

    tamerrrr

--- src/select.c
+++ src/select.c
@@ -13,76 +13,109 @@
 #include "libft.h"
 #include <stdio.h>
 #include <term.h>
-#include <termios.h>
-#include <curses.h>
-#include <sys/ioctl.h>
 
-typedef enum	e_caps
+#define TRM_DEV "/dev/tty"
+#define TRM_USG "%s: Specify a terminal type with `setenv TERM <yourtype>`\n"
+
+typedef TTY		t_tty;
+
+typedef struct	s_trm
 {
-	CAPS_CL = 0,
-	CAPS_CM,
-	CAPS_SO,
-	CAPS_SE,
-	CAPS_KR,
-	CAPS_VI,
-	CAPS_TI
-}				t_caps;
+	t_tty		tty;
+	int			height;
+	int			width;
+}				t_trm;
 
-typedef TTY		t_termios;
-typedef struct winsize	t_wsz;
-
-typedef struct	s_term
+t_st	ft_trm_ctor(t_trm *self)
 {
-	char		*caps[7];
-	t_termios	ios;
-	t_wsz		wsz;
-}				t_term;
+	char	*tnm;
 
-t_st	ft_term_ctor(t_term *self)
-{
-	char *term;
-
-	FT_INIT(self, t_term);
-	if (!(term = getenv("TERM")))
-		return (ft_ret(NOK, "%s: Set the env TERM var\n", "select"));
-	if (tgetent(NULL, term) == -1)
-		return (ft_ret(NOK, "%s: %e\n", "select", errno));
-	self->caps[CAPS_CL] = tgetstr("cl", NULL);
-	self->caps[CAPS_CM] = tgetstr("cm", NULL);
-	self->caps[CAPS_SO] = tgetstr("so", NULL);
-	self->caps[CAPS_SE] = tgetstr("se", NULL);
-	self->caps[CAPS_KR] = tgetstr("kr", NULL);
-	self->caps[CAPS_VI] = tgetstr("vi", NULL);
-	self->caps[CAPS_TI] = tgetstr("ti", NULL);
-	tcgetattr(0, &self->ios);
-	self->ios.c_lflag &= ~ICANON;
-	self->ios.c_lflag &= ~ECHO;
-	self->ios.c_cc[VMIN] = 1;
-	self->ios.c_cc[VTIME] = 100;
+	FT_INIT(self, t_trm);
+	if (!(tnm = getenv("TERM")))
+		return (ft_passf(NOK, TRM_USG, "select"));
+	if (tgetent(NULL, tnm) <= 0 )
+		return (ft_passf(NOK, "%s: %e.\n", errno));
+	self->height = tgetnum("li");
+	self->width = tgetnum("co");
+	tcgetattr(0, &self->tty);
+	self->tty.c_lflag &= ~ICANON;
+	self->tty.c_lflag &= ~ECHO;
+	self->tty.c_cc[VMIN] = 1;
+	self->tty.c_cc[VTIME] = 100;
 	return (OK);
+}
+
+typedef enum	s_trm_mv
+{
+	TRM_UP = 0,
+	TRM_DOWN = 2,
+	TRM_LEFT = 4,
+	TRM_RIGHT = 6
+}				t_trm_mv;
+
+static char		*g_trm_mvs[8] =
+{
+	[TRM_UP] = "up",
+	[TRM_UP + 1] = "UP",
+	[TRM_DOWN] = "do",
+	[TRM_DOWN + 1] = "DO",
+	[TRM_LEFT] = "le",
+	[TRM_LEFT + 1] = "LE",
+	[TRM_RIGHT] = "nd",
+	[TRM_RIGHT + 1] = "RI"
+};
+
+void	ft_trm_mv(t_trm_mv mv, int n)
+{
+	if (n == 1)
+		tputs(tgetstr(g_trm_mvs[mv], NULL), 1, putchar);
+	else if (n)
+		tputs(tgoto(tgetstr(g_trm_mvs[mv + 1], NULL), 0, n), 1, putchar);
+}
+
+void	ft_trm_goto_x(int x)
+{
+	tputs(tgoto(tgetstr("ch", NULL), 0, x), 1, putchar);
+}
+
+void	ft_trm_goto_y(int y)
+{
+	tputs(tgoto(tgetstr("cv", NULL), 0, y), 1, putchar);
+}
+
+void	select_print(t_trm *t, int ac, char **av, int pad)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	k = 0;
+	pad += 4;
+	while (i < t->height && k < ac - 1)
+	{
+		j = 0;
+		while (j < (t->width / pad) && k < ac - 1)
+		{
+			ft_trm_goto_x(j * pad);
+			tputs("[ ", 1, putchar);
+			ft_trm_goto_x((j * pad) + 2);
+			tputs(av[++k], 1, putchar);
+			ft_trm_goto_x((j * pad) + pad - 1);
+			tputs("]", 1, putchar);
+			++j;
+		}
+		ft_trm_mv(TRM_DOWN, 1);
+		++i;
+	}
 }
 
 int		main(int ac, char **av)
 {
-	int		i;
-	t_term	term;
-	char	buf[4];
+	t_trm		trm;
 
-	if (ST_NOK(ft_term_ctor(&term)))
+	if (ST_NOK(ft_trm_ctor(&trm)))
 		return (EXIT_FAILURE);
-	tputs(term.caps[CAPS_VI], 1, putchar);
-	tputs(term.caps[CAPS_TI], 1, putchar);
-	while (1)
-	{
-		tputs(term.caps[CAPS_CL], 1, putchar);
-		i = 0;
-		while (++i < ac)
-		{
-			ft_puts(1, av[i]);
-			ft_putc(1, '\n');
-		}
-		bzero(buf, 4);
-		read(0, buf, 4);
-		ioctl(0, TIOCGWINSZ, &term.wsz);
-	}
+	select_print(&trm, ac, av, 15);
+	return (EXIT_SUCCESS);
 }
